2f17045d43fa9d59ed4f17c374a107173b1feba8
Wincent Balin <wincent.balin@gmail.com>
2021-08-10 21:45:49 +0200

    Updated copyright

--- install-opengrm.sh
+++ install-opengrm.sh
@@ -2,7 +2,7 @@
 #
 # Download and install OpenFST with contributions and all OpenGRM projects
 #
-# Copyright 2018-2019 Wincent Balin
+# Copyright 2018-2021 Wincent Balin
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
